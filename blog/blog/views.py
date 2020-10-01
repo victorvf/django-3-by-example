a1e856c4e1eeea209f0766087016bb7bdfd297f5
victorvf <victorvitoria11@hotmail.com>
2020-09-30 22:13:47 -0300

    feature: first views

--- blog/blog/views.py
+++ blog/blog/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
-# Create your views here.
+from .models import Post
+
+
+def post_list(request):
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 3)
+    page = request.GET.get('page')
+
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
+    return render(
+        request,
+        'blog/post/post_list.html',
+        {'posts': posts, 'page': page},
+    )
+
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(
+        Post,
+        slug=post,
+        status='published',
+        publish__year=year,
+        publish__month=month,
+        publish__day=day,
+    )
+
+    return render(request, 'blog/post/post_detail.html', {'post': post})
